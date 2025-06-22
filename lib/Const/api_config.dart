16a0dfc097d287518fb9a3314573ad5eb7775e67
Xaina Tech Pvt. Ltd. <147532222+organkha@users.noreply.github.com>
2025-06-22 10:18:18 +0545

    Delete lib directory

--- lib/Const/api_config.dart
+++
@@ -1,6 +0,0 @@
-class APIConfig {
-  static String domain = 'https://afnoledger.sangam69.com.np/';
-  static String url = '${domain}api/v1';
-  static String registerUrl = '/sign-up';
-  static String businessCategoriesUrl = '/business-categories';
-}
